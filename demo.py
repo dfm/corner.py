928cda780ae730a2ba3178b0f004fb98012e86e5
Geoff Ryan <geoffrey.s.ryan@gmail.com>
2013-01-05 15:09:14 -0700

    reverted tabs to spaces

--- demo.py
+++ demo.py
@@ -7,8 +7,8 @@ import triangle
 
 
 if __name__ == "__main__":
-	data = np.random.randn(5000 * 4).reshape([4, 5000])
-	extents = [[-i, i] for i in range(1, 5)]
-	triangle.corner(data, labels=["var 1", "var 2", "var 3", "var 4"],
-					extents=extents)
-	pl.savefig("demo.png")
+    data = np.random.randn(5000 * 4).reshape([4, 5000])
+    extents = [[-i, i] for i in range(1, 5)]
+    triangle.corner(data, labels=["var 1", "var 2", "var 3", "var 4"],
+                    extents=extents)
+    pl.savefig("demo.png")
