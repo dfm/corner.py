4742a29049f4d07ca35a299c0c8c37507762ad55
Dan F-M <danfm@nyu.edu>
2013-07-30 11:01:10 +0200

    fix datatype error

--- triangle.py
+++ triangle.py
@@ -120,7 +120,8 @@ def corner(xs, labels=None, extents=None, truths=None, truth_color="#4682b4",
             raise ValueError(("It looks like the parameter(s) in column(s) "
                               "{0} have no dynamic range. Please provide an "
                               "`extent` argument.")
-                             .format(",".join(np.arange(len(m))[m])))
+                             .format(",".join(map(unicode,
+                                                  np.arange(len(m))[m]))))
 
     for i, x in enumerate(xs):
         ax = axes[i, i]
