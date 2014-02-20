0b19f8a00b2b41d7dd5f1e72e9c6da53998d4210
Adrian Price-Whelan <adrian.prw@gmail.com>
2014-02-20 10:21:49 -0500

    update demo

--- demo.py
+++ demo.py
@@ -11,15 +11,30 @@ data1 = np.random.randn(ndim * 4 * nsamples / 5).reshape([4 * nsamples / 5,
                                                           ndim])
 data2 = (5 * np.random.rand(ndim)[None, :]
          + np.random.randn(ndim * nsamples / 5).reshape([nsamples / 5, ndim]))
-data = np.vstack([data1, data2])
+test_data = np.vstack([data1, data2])
+
+# Generate some more fake data.
+data1 = np.random.randn(ndim * 4 * nsamples / 5).reshape([4 * nsamples / 5,
+                                                          ndim])
+data2 = (5 * np.random.rand(ndim)[None, :]
+         + np.random.randn(ndim * nsamples / 5).reshape([nsamples / 5, ndim]))
+test_data2 = np.vstack([data1, data2])
 
 # Plot it.
-figure = triangle.corner(data, labels=[r"$x$", r"$y$", r"$\log \alpha$",
-                                       r"$\Gamma \, [\mathrm{parsec}]$"],
-                         truths=[0.0, 0.0, 0.0],
-                         quantiles=[0.16, 0.5, 0.84],
-                         show_titles=True, title_args={"fontsize": 12})
-figure.gca().annotate("A Title", xy=(0.5, 1.0), xycoords="figure fraction",
+fig = triangle.corner(test_data, labels=[r"$x$", r"$y$", r"$z$"],
+                      truths=[0.5, 0.5, 0.45],
+                      quantiles=[0.16, 0.5, 0.84],
+                      plot_contours=True, plot_datapoints=False,
+                      contour_kwargs=dict(linewidths=3.,alpha=0.5),
+                      hist_kwargs=dict(normed=True,bins=50))
+
+fig = triangle.corner(test_data2, fig=fig,
+                      quantiles=[0.16, 0.5, 0.84],
+                      plot_contours=False, plot_datapoints=True,
+                      point_kwargs=dict(color='#2b8cbe',markersize=4,alpha=0.02),
+                      hist_kwargs=dict(color='#2b8cbe',bins=100,normed=True))
+
+fig.gca().annotate("A Title", xy=(0.5, 1.0), xycoords="figure fraction",
                       xytext=(0, -5), textcoords="offset points",
                       ha="center", va="top")
-figure.savefig("demo.png")
+fig.savefig("demo.png")
