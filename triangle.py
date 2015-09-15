d3323b15cb75b7e6aeb05b298e4b89a802521535
Dan F-M <danfm@nyu.edu>
2015-09-15 15:33:01 -0700

    import error

--- triangle.py
+++ triangle.py
@@ -6,7 +6,12 @@ __all__ = ["corner", "hist2d"]
 __version__ = "0.3.0"
 
 import logging
-from corner import corner, hist2d
+
+try:
+    from corner import corner, hist2d
+except ImportError:
+    raise ImportError("'triangle' has been renamed to 'corner'. Install that "
+                      "module: https://github.com/dfm/corner.py")
 
 logging.warn("Deprecation Warning: 'triangle' has been renamed to 'corner'. "
              "This shim should continue to work but you should use 'import "
