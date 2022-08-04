0cb9cf9e3886fddf00ad99a1a9c780281f85c431
Dan F-M <foreman.mackey@gmail.com>
2022-08-04 19:11:19 -0400

    less setup

--- setup.py
+++ setup.py
@@ -1,8 +1,3 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-setup(
-    name="corner",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
-    include_package_data=True,
-)
+setup()
