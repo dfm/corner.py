b778863a867d6b69743b444c895174348799d273
Gabriel Brammer <gbrammer@gmail.com>
2025-12-04 16:40:38 +0100

    add function to init

--- src/corner/__init__.py
+++ src/corner/__init__.py
@@ -2,6 +2,6 @@
 
 __all__ = ["corner", "hist2d", "quantile", "overplot_lines", "overplot_points"]
 
-from corner.core import hist2d, overplot_lines, overplot_points, quantile
+from corner.core import hist2d, overplot_lines, overplot_points, get_axis_by_index, quantile
 from corner.corner import corner
 from corner.version import version as __version__
